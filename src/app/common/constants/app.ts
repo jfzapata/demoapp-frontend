f7f298969755bbc10708001c63480fefb6de6c0d
Johan Zapata <jfzr9698@gmail.com>
2019-04-02 01:40:02 -0500

    Se crea archivo con constantes de la aplicación

---
+++ src/app/common/constants/app.ts
@@ -0,0 +1,4 @@
+// Own
+import { environment } from '@root/environments/environment';
+
+export const API_URL = environment.API_URL;
--- src/app/common/services/users.service.ts
+++ src/app/common/services/users.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Own
+// Constants
+import { API_URL } from '@app/common/constants/app';
 
 @Injectable({
   providedIn: 'root'
