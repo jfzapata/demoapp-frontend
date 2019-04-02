4833e122ce85361e6ec0ef1e451b3ee67db642d0
Johan Zapata <jfzr9698@gmail.com>
2019-04-02 05:43:39 -0500

    Se consultan los usuarios

--- src/app/core/components/tarea-create/tarea-create.component.ts
+++ src/app/core/components/tarea-create/tarea-create.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+// Own
+// Types
+import { Usuario } from '@app/common/types/interfaces/usuario';
+import { Tarea } from '@app/common/types/interfaces/tarea';
+// Services
+import { UsuariosService } from '@app/common/services/usuarios.service';
+import { TareasService } from '@app/common/services/tareas.service';
+
 @Component({
   selector: 'app-tarea-create',
   templateUrl: './tarea-create.component.html',
@@ -7,9 +15,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareaCreateComponent implements OnInit {
   isEdition = false; // is edition?
-  constructor() { }
+  usuarios: Usuario[];
+  constructor(private usuariosService: UsuariosService) { }
 
   ngOnInit() {
+    this.usuariosService.getUsuarios()
+    .subscribe((usuarios: Usuario[]) => {
+      this.usuarios = usuarios;
+    });
   }
 
 }
