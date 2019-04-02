2964ae178662c837f17689b5d893b8b6735ea01d
Johan Zapata <jfzr9698@gmail.com>
2019-04-02 02:13:21 -0500

    Se consultan usuarios en componente de lista de usuarios

--- src/app/core/components/usuarios-list/usuarios-list.component.ts
+++ src/app/core/components/usuarios-list/usuarios-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 // Own
+// Types
+import { Usuario } from '@app/common/types/interfaces/usuario';
 // Services
 import { UsuariosService } from '@app/common/services/usuarios.service';
 
@@ -10,10 +12,21 @@ import { UsuariosService } from '@app/common/services/usuarios.service';
   styleUrls: ['./usuarios-list.component.scss']
 })
 export class UsuariosListComponent implements OnInit {
-
+  usuarios: Usuario[]; // The records gonna be show
   constructor(private usuariosService: UsuariosService) { }
 
   ngOnInit() {
+    this.getUsuarios();
+  }
+
+  private getUsuarios() {
+    this.usuariosService.getUsuarios()
+    .subscribe((usuarios: Usuario[]) => {
+      this.usuarios = usuarios;
+      console.log(this.usuarios);
+    }, (err) => {
+
+    });
   }
 
 }
