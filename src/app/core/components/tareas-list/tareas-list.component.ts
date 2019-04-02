3ef8fbcd32978b3a087bd8bde3ad677223ec9db2
Johan Zapata <jfzr9698@gmail.com>
2019-04-02 02:16:26 -0500

    Se consultan tareas en componente de lista de tareas

--- src/app/core/components/tareas-list/tareas-list.component.ts
+++ src/app/core/components/tareas-list/tareas-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 // Own
+// Types
+import { Tarea } from '@app/common/types/interfaces/tarea';
 // Services
 import { TareasService } from '@app/common/services/tareas.service';
 
@@ -10,10 +12,21 @@ import { TareasService } from '@app/common/services/tareas.service';
   styleUrls: ['./tareas-list.component.scss']
 })
 export class TareasListComponent implements OnInit {
-
+  tareas: Tarea[]; // The records gonna be show
   constructor(private tareasService: TareasService) { }
 
   ngOnInit() {
+    this.getTareas();
+  }
+
+  private getTareas() {
+    this.tareasService.getTareas()
+    .subscribe((tareas: Tarea[]) => {
+      this.tareas = tareas;
+      console.log(this.tareas);
+    }, (err) => {
+
+    });
   }
 
 }
